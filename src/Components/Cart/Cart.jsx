87cf5787e554a6459b88a41ee2919d3ab520083a
Mark Lim <mark.ljr2@gmail.com>
2023-04-20 21:33:44 +0800

    stub up cart to main page

---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = () => {
+    return (
+        <div className='Cart'>
+            <h1>Placeholder Cart</h1>
+        </div>
+    );
+};
+
+export default Cart;
--- src/Pages/MainStore/MainStore.jsx
+++ src/Pages/MainStore/MainStore.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import NavBar from '../../Components/NavBar/NavBar';
 import ProductGrid from '../../Components/Product/ProductGrid';
+import Cart from '../../Components/Cart/Cart';
+import './MainStore.css'
 
 const MainStore = () => {
     return (
 
-        <div>
+        <div className='MainStore'>
             <NavBar />
                 <h1>Main Store Page</h1>
+                <Cart />
                 <ProductGrid />
+                
+                
         </div>
     );
 };
