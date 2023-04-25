6fa8f699d2debfacdf8f2be912fd3dd53b4a4e0f
Mark Lim <mark.ljr2@gmail.com>
2023-04-25 21:24:04 +0800

    updated edit product to include alt img + css

--- src/Components/ModifyProduct/ModifyProduct.jsx
+++ src/Components/ModifyProduct/ModifyProduct.jsx
@@ -118,6 +118,103 @@ const ModifyProduct = () => {
                     </label>
                     <br></br>
                     <button type="submit" onClick={handleUpdate}>Update Product Information</button>
+
+              <div className="bg-grey-lighter min-h-screen flex flex-col">
+                <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                    <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                        <h1 className="mb-8 text-3xl text-center"> Update Product Information </h1>
+                    <form>
+                        <input 
+                            type="text"
+                            className="block border border-grey-light w-full p-3 rounded mb-4"
+                            name="bookImg"
+                            label="Book Image"
+                            onChange={handleChange}
+                            value={books.bookImg}
+                            required 
+                            placeholder="Book Img Url" />
+                        <input 
+                            type="text"
+                            className="block border border-grey-light w-full p-3 rounded mb-4"
+                            name="bookAltImg"
+                            label="Book Alt Image"
+                            onChange={handleChange}
+                            value={books.bookAltImg}
+                            required 
+                            placeholder="Book Alt Img Url" />   
+                        <input 
+                            type="text"
+                            className="block border border-grey-light w-full p-3 rounded mb-4"
+                            name="bookTitle"
+                            label="Book Title"
+                            onChange={handleChange}
+                            value={books.bookTitle}
+                            required 
+                            placeholder="Book Title" />
+
+                        <input 
+                            type="text"
+                            className="block border border-grey-light w-full p-3 rounded mb-4"
+                            name="author"
+                            label="Author"
+                            onChange={handleChange}
+                            value={books.author}
+                            required 
+                            placeholder="Author" /> 
+
+                        <input 
+                            type="text"
+                            className="block border border-grey-light w-full p-3 rounded mb-4"
+                            name="publisher"
+                            label="Publisher"
+                            onChange={handleChange}
+                            value={books.publisher}
+                            required 
+                            placeholder="Publisher" /> 
+
+
+                        <label className="block mb-2 text-sm font-medium text-gray-400">Book Description</label>
+                        <textarea type="text" name="writeUp" onChange={handleChange} value={books.writeUp} id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+
+                        <input 
+                            type="number"
+                            className="block border border-grey-light w-full p-3 rounded mb-4"
+                            name="price"
+                            label="Price"
+                            onChange={handleChange}
+                            value={books.price}
+                            required 
+                            placeholder="Price" /> 
+
+                        <input 
+                            type="number"
+                            className="block border border-grey-light w-full p-3 rounded mb-4"
+                            name="quantity"
+                            label="Quantity"
+                            onChange={handleChange}
+                            value={books.quantity}
+                            required 
+                            placeholder="Quantity" /> 
+
+                        <input 
+                            type="text"
+                            className="block border border-grey-light w-full p-3 rounded mb-4"
+                            name="tags"
+                            label="Tags"
+                            onChange={handleChange}
+                            value={books.tags}
+                            required 
+                            placeholder="Tags, seperate tag with ," /> 
+
+                        <button
+                            type="submit"
+                            onClick={handleUpdate}
+                            className="w-full text-center py-3 rounded bg-green text-black hover:bg-green-dark focus:outline-none my-1"         
+                            >Submit New Product</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
             
         </div>
     );
--- src/Components/ModifyProduct/ProductIndex.jsx
+++ src/Components/ModifyProduct/ProductIndex.jsx
@@ -30,20 +30,22 @@ const ProductIndex = () => {
 
     return (
         <div>
-            <h1> Product Index</h1>
+            <h1 className='flex justify-center items-center py-4 text-xl'> Product Index</h1>
             <div>
             {books.map((book)=> (
-            <ul key={book._id} className='bookIndex'>
-                <li>Book Title : {book.bookTitle}</li>
-                <li>Author : {book.author}</li>
-                <li>Price : ${book.price}</li>
-                <li>Quantity : {book.quantity}</li>
+            <div key={book._id}>
+                <h5 className='inline-block px-2'>Book Title : {book.bookTitle}</h5>
+                <h5 className='inline-block px-2'>Author : {book.author}</h5>
+                <h5 className='inline-block px-2'>Price : ${book.price}</h5>
+                <h5 className='inline-block px-2'>Quantity : {book.quantity}</h5>
                 <Link to={`/admin/${book._id}/edit`} >
-                <button>Edit</button>
+                <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-2 py-2 mr-2 mb-2">
+                  Edit</button>
                 </Link>
-                <button onClick={() => handleDelete(artwork._id)}>Delete</button>
-            
-            </ul> 
+                <button onClick={() => handleDelete(artwork._id)}
+                className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2 mr-2 mb-2"
+                >Delete</button>
+            </div> 
             ))}
 
             </div>
